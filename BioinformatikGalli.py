e2e040f537b1adc129ef358abb0115f6e6bc704e
christophergalli <39825397+christophergalli@users.noreply.github.com>
2018-12-31 11:23:59 +0100

    Update BioinformatikGalli.py

--- BioinformatikGalli.py
+++ BioinformatikGalli.py
@@ -2,6 +2,11 @@ from reportlab.lib import colors
 from reportlab.lib.units import cm
 from Bio.Graphics import GenomeDiagram
 from Bio import SeqIO
+import pandas as pd
+from collections import Counter
+import numpy as np
+from collections import defaultdict
+from itertools import chain
 from Bio.SeqFeature import SeqFeature, FeatureLocation
 from Bio import SeqFeature
 
@@ -30,49 +35,91 @@ interested_gene = ['mRNA']  # gene
 interested_product = ['tRNA', 'rRNA']  # product
 
 
+#test_dict = SeqIO.to_dict(SeqIO.parse("vectors-100.gb", "genbank"))
+
+
 record = SeqIO.parse(inputFile, "genbank")
+
+#[featureDict.__setitem__(item, 1+featureDict.get(item, 1)) for item in interested_only_note]
+
+x = []
+testDict = dict()
+
 
 
 for search in record:
     if len(search.seq) >= 1500:
         for feature in search.features:
             if feature.type in interested_only_note:
-                featureDict[feature.location.extract(search).seq] = (feature.type, feature.qualifiers['note'])
+                try:
+                    featureDict[feature.location.extract(search).seq] = (feature.type, feature.qualifiers['note'])
+                    a = feature.type + " " + feature.location.extract(search).seq
+                    x.append(a)
 
+
+
+
+
+
+                except:
+                    break
+                    #print("Couldn't finde any gene or note in ", interested_only_note)
 
             if feature.type in interested_note_and_gen:
                 try:
                     featureDict[feature.location.extract(search).seq] = (feature.type, feature.qualifiers['note'],
                                                                      feature.qualifiers['gene'])
+
+
                 except:
-                    print("Couldn't find any gene or note in", interested_note_and_gen)
+                    break
+                    #print("Couldn't find any gene or note in", interested_note_and_gen)
+
             if feature.type in interested_gene_and_product:
                 try:
                     featureDict[feature.location.extract(search).seq] = (feature.type, feature.qualifiers['product'],
                                                                  feature.qualifiers['gene'])
+                    a = feature.type + " " + feature.location.extract(search).seq
+                    x.append(a)
+
                 except:
-                    print ("Couldn't find any product or gene in ", interested_gene_and_product)
+                    break
+                    #print ("Couldn't find any product or gene in ", interested_gene_and_product)
 
             if feature.type in interested_note_and_bound_moiety:
                 try:
                     featureDict[feature.location.extract(search).seq] = (feature.type, feature.qualifiers['note'],
                                                                  feature.qualifiers['bound_moiety'])
+                    a = feature.type + " " + feature.location.extract(search).seq
+                    x.append(a)
+
                 except:
-                    print ("Coulnd't find any note or bound_moiety in ", interested_note_and_bound_moiety)
+                    break
+                    #print ("Coulnd't find any note or bound_moiety in ", interested_note_and_bound_moiety)
 
             if feature.type in interested_gene:
                 try:
                     featureDict[feature.location.extract(search).seq] = (feature.type, feature.qualifiers['gene'])
+                    a = feature.type + " " + feature.location.extract(search).seq
+                    x.append(a)
                 except:
-                    print("Couldn't find any gene in ", interested_gene)
+                    break
+                    #print("Couldn't find any gene in ", interested_gene)
 
             if feature.type in interested_product:
                 try:
                     featureDict[feature.location.extract(search).seq] = (feature.type, feature.qualifiers['product'])
+                    a = feature.type + " " + feature.location.extract(search).seq
+                    x.append(a)
                 except:
-                    print("Couldn't find any product in ", interested_product)
-
+                    break
+                    #print("Couldn't find any product in ", interested_product)
 print(featureDict)
 print(len(featureDict))
 
+print(len(testDict))
+
+c = Counter(x)
+print(c)
+print(len(c))
 
